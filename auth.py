61a7d469318794ca044180eef6557eea0b27b023
chawasit <t.turtle_no.1@hotmail.com>
2016-02-07 16:27:40 +0700

    Add Auth/login

--- auth.py
+++ auth.py
@@ -11,7 +11,7 @@ auth_api = Blueprint('auth_api', __name__)
 
 @auth_api.route('/auth', methods=['PUT', 'POST'])
 def auth():
-    if request.method == 'POST':
+    if request.method == 'POST': # Login
         username = request.form["username"]
         password = GetHash( request.form["password"] )
         user = auth_db.find_one({
@@ -20,8 +20,22 @@ def auth():
                         })
         if not user:
             return jsonify(msg="Access Denied"), 401
-        token = 123
+        token = 123 # Generate new Token or some OAuth Spec
+        result auth_db.update_one({"username":username},{
+                "$set": {
+                    "token": token
+                },
+                "$currentDate": {"lastModified": True}
+            })
+        if result.modified_count != 1:
+            return jsonify(msg="Try Again Later"), 500
         return jsonify(token=token)
-    if request.method == 'PUT':
-        # Register
-        pass
+        
+    if request.method == 'PUT': # Register
+        username = request.form["username"]
+        password = GetHash( request.form["password"] )
+        result = auth_db.insert_one({"username":username, "password":password})
+        if not result:
+            return jsonify(msg="Try Again Later"), 500
+        return jsonify(msg="OK")
+        
