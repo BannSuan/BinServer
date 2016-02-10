0e7ff8fdb846e9dea94c916a39f1c5efa9e7edd6
Bongtrop <bongtrop@gmail.com>
2016-02-11 03:47:26 +0700

    create user controller

--- model/users.py
+++ model/users.py
@@ -9,14 +9,22 @@ from pymongo import MongoClient
 from config import Config
 
 client = MongoClient()
-
 users = client[Config['DATABASE']]["users"]
 
 def login(token):
 	return users.find_one({"token": token})
 
-def add(token, name, fullname, email, image):
+def add(token, name="", fullname="", email="", image=""):
 	users.insert_one({"name": name, "fullname": fullname, "email": email, "image": image, "token": token, "create_ts": time.time()})
 
-def get(search, skip=0, limit=10, s=1):
+def update(token, name, fullname, email, image):
+	return users.update_one({"name": name, "fullname": fullname, "email": email, "image": image}, {"token": token}).modified_count==1
+
+def search(search, skip=0, limit=10):
+	if limit>50:
+		limit = 50
+
 	return list(users.find(filter={"$or": [{"name": '/'+search+'/'}, {"fullname": '/'+search+'/'}, {"email": '/'+search+'/'}]}, skip=skip, limit=limit).sort('name', pymongo.DESCENDING))
+
+def get(uid):
+	return users.find_one({"_id": uid})
--- user.py
+++ user.py
@@ -7,20 +7,58 @@
 """
 
 from flask import Blueprint, abort, request, session, jsonify
-from db import KeyStore
+from db import KeyStore, GenerateKey
+import model.users
 
 user_api = Blueprint('user_api', __name__)
 
 @user_api.route('/user', methods=['GET', 'POST', 'PUT'])
+@user_api.route('/user/<uid>', methods=['GET'])
 def user():
     key = request.headers.get('x-key')
     user = KeyStore.search(key)
+
+    if uid is not None:
+
+        if uid=="me":
+            uid = user["_id"]
+
+        res = model.users.get(uid)
+        return jsonify(status="ok", message="", data=res)
 
     if request.method == 'GET':
         if not user:
             abort(401)
 
-        return jsonify(status="ok", message="", data=[])
+        search = request.args.get("search", "")
+        skip = request.args.get("skip", 0)
+        limit = request.args.get("limit", 10)
+
+        res = model.users.search(search, skip=skip, limit=limit)
+
+        return jsonify(status="ok", message="", data=res)
 
     elif request.method == 'POST':
-        token = request.form.args["token"]
+        token = request.form["token"]
+        user = model.users.login(token)
+
+        if user:
+            key = GenerateKey()
+            KeyStore.save(key, user)
+            user["key"] = key
+
+            return jsonify(status="ok", message="", data=user)
+        else:
+            model.users.add(token)
+            return jsonify(status="ok", message="news", data={})
+
+    elif request.method == 'PUT':
+        token = request.form["token"]
+        name = request.form["name"]
+        fullname = request.form["fullname"]
+        image = request.form["image"]
+
+        if model.users.update(token, name, fullname, image):
+            return jsonify(status="ok", message="", data={})
+        else:
+            abort(404)
